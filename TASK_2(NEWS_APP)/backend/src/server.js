084cf7fb2e91246b2db3a8ff08ae661d22355a2f
Shiven Sharma <shiven676@gmail.com>
2026-06-09 20:58:38 +0530

    build_Task2: Added Navbar in Frontend and setting up Backend

--- TASK_2(NEWS_APP)/backend/src/server.js
+++ TASK_2(NEWS_APP)/backend/src/server.js
@@ -1,3 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+
+
+PORT = process.env.PORT || 5000;
+
+const app = express () ;
+
+app.listen(process.env.PORT,()=>{
+    console.log(`server is running at ${PORT}`);
+}    
+)
