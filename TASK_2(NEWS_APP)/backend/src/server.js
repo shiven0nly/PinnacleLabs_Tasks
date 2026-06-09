90cbe872388c571b661349bba7743c1acf67adb3
Shiven Sharma <shiven676@gmail.com>
2026-06-09 21:06:31 +0530

    feat: initialize backend project with Express server and essential dependencies

--- TASK_2(NEWS_APP)/backend/src/server.js
+++ TASK_2(NEWS_APP)/backend/src/server.js
@@ -2,12 +2,21 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
+dotenv.config();
 
-PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-const app = express () ;
+const app = express();
 
-app.listen(process.env.PORT,()=>{
-    console.log(`server is running at ${PORT}`);
-}    
-)
+//MiddleWare
+app.use(express.json());
+app.use(cors());
+
+// Health check endpoint
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'UP', timestamp: new Date() });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`server is running at ${PORT}`);
+});
