e7cf2a29d4a27a937db3dafb55ff50c4b278e2f8
Shiven Sharma <shiven676@gmail.com>
2026-06-09 18:53:52 +0530

    build_Task2: setup routes in app.jsx

--- TASK_2(NEWS_APP)/frontend/src/App.jsx
+++ TASK_2(NEWS_APP)/frontend/src/App.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { SignIn } from './auth/forms/SignIn';
+import { SignUp } from './auth/forms/SignUp';
+import { NewsArticles } from './pages/NewsArticles';
+import { About } from './pages/About';
+import { Dashboard } from './pages/Dashboard';
 
 const App = () => {
-  return <></>;
+  return <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/news-articles" element={<NewsArticles />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  </>;
 };
 
 export default App;
