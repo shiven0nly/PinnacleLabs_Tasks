8bc6f824523ac74833db758a9fd1316e2398f5bd
Shiven Sharma <shiven676@gmail.com>
2026-06-11 00:51:38 +0530

    feat: initialize Redux store with persistence and integrate Provider in main entry point

--- TASK_2(NEWS_APP)/frontend/src/main.jsx
+++ TASK_2(NEWS_APP)/frontend/src/main.jsx
@@ -4,9 +4,13 @@ import './index.css';
 import App from './App.jsx';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor } from './redux/store';
 
 createRoot(document.getElementById('root')).render(
+  <PersistGate persistor={persistor}>
   <Provider store={store}>
     <App />
   </Provider>
+  </PersistGate>
 );
